e3256e36abf7099b3529e2161f8165f5d3c70b80
Marshall Huss <mwhuss@gmail.com>
2010-02-18 12:54:05 -0500

    Fixes

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,18 +1,14 @@
 #ENV["GEM_HOME"] = File.expand_path('../../vendor/bundler_gems', __FILE__)
 
-if ENV['RAILS_ENV'] == 'development'
-
-  begin
-    # Require the preresolved locked set of gems.
-    require File.expand_path('../../.bundle/environment', __FILE__)
-  rescue LoadError
-    raise RuntimeError, "You have not locked your bundle. Run `bundle lock`."
+begin
+  # Require the preresolved locked set of gems.
+  require File.expand_path('../../.bundle/environment', __FILE__)
+rescue LoadError
+  raise RuntimeError, "You have not locked your bundle. Run `bundle lock`."
  
-    # Fallback on doing the resolve at runtime.
-    # This does not work with rails 2.3.5 as of bundler 0.9.5.
-    require "rubygems"
-    require "bundler"
-    Bundler.setup
-  end
-  
+  # Fallback on doing the resolve at runtime.
+  # This does not work with rails 2.3.5 as of bundler 0.9.5.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
 end
