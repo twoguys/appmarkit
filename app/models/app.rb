1faabb2da87f8999fd0d2cb09963008cd3d06de6
Marshall Huss <mwhuss@gmail.com>
2010-02-20 18:42:40 -0500

    Added itunes information

--- app/models/app.rb
+++ app/models/app.rb
@@ -14,6 +14,8 @@ class App < ActiveRecord::Base
   validates_exclusion_of  :subdomain, :in => %w( www admin )
   validates_format_of     :subdomain, :with => /^[a-z0-9-]+$/
   
+  validates_presence_of   :itunes_url
+  
   has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   has_attached_file :icon, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   
@@ -28,6 +30,10 @@ class App < ActiveRecord::Base
     self.description      = itunes.description
     self.subdomain        = self.name.slugify
     self.itunes_url       = itunes.track_view_url
+  end
+  
+  def affiliate_url
+    "http://click.linksynergy.com/fs-bin/stat?id=UjB6GWcy1A8&offerid=146261&type=3&subid=0&tmpid=1826&RD_PARM1=#{self.itunes_url}&partnerId=30"
   end
   
 end
