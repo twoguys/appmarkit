6b54d2ed4b815d877830ca5e4873657a9d74c2c9
Marshall Huss <mwhuss@gmail.com>
2010-02-22 15:12:52 -0500

    Added cache control to sites

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -3,10 +3,12 @@ class SitesController < ApplicationController
   before_filter :app_required
   
   def index
+    response.headers['Cache-Control'] = 'public, max-age=300'
     render :layout => false
   end
 
   def itunes
+    #response.headers['Cache-Control'] = 'public, max-age=300'
     render :layout => false
   end
   
