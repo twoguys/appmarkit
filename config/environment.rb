d99483e7673603b621efc9b3c883a56822b22b7d
Marshall Huss <mwhuss@gmail.com>
2010-02-22 15:34:59 -0500

    removing authlogic oauth gem

--- config/environment.rb
+++ config/environment.rb
@@ -25,7 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "authlogic",            :version => "2.1.3"
   config.gem "heroku",               :version => "1.8.0"
   config.gem "acts-as-list",         :version => "0.1.2",       :lib => "acts_as_list"
-  config.gem "authlogic-oauth",      :version => "1.0.8",       :lib => "authlogic_oauth"
+  #config.gem "authlogic-oauth",      :version => "1.0.8",       :lib => "authlogic_oauth"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
